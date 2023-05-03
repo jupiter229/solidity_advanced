8b347c5133211425833a6343f7bf41b510475b56
jupiter229 <smartwang1004@outlook.com>
2023-05-02 20:18:44 -0700

    feat: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,19 +3,20 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 dotenv.config()
 
-const accountKey = process.env.PRIVATE_KEY || "";
+const { API_URL, PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   defaultNetwork: "goerli",
   networks: {
     hardhat: {
     },
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [accountKey]
+      url: `${API_URL}/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
   etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+    apiKey: `${ETHERSCAN_API_KEY}`,
   },
   solidity: {
     version: "0.8.17",
